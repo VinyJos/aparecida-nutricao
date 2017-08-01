20d99959c984bc86f2af789eb8b048376a536cf1
Paulo Cesar Ferreira de Mello <paulo@javascript.floripa.br>
2017-07-31 22:19:04 -0300

    Validação de dados e operadores
    Foram feitas validações de campos com mensagens, manipulação de dados
    pelo DOM e também trabalhamos com operadores.

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,48 @@
-var titulo = document.querySelector('.titulo');
-titulo.textContent = 'Aparecida Nutricionista';
+(function Principal() {
+    var titulo = document.querySelector('.titulo');
+    titulo.textContent = 'Aparecida Nutricionista';
+
+    var paciente = document.querySelector('#primeiro-paciente');
+    var dadosPaciente = obtemDadosDoPaciente(paciente);
+    var infoImc = paciente.querySelector('.info-imc');
+
+    if (!validaPeso(dadosPaciente.peso)) {
+        var mensagem = "Peso inválido";
+        console.log(mensagem);
+        infoImc.textContent = mensagem;
+        return;
+    }
+    if(!validaAltura(dadosPaciente.altura)) {
+        var mensagem = "Altura inválida";
+        console.log(mensagem);
+        infoImc.textContent = mensagem;
+        return;
+    }
+
+    dadosPaciente.imc = calculaImc(dadosPaciente.peso, dadosPaciente.altura);
+    paciente.querySelector('.info-imc').textContent = dadosPaciente.imc;
+
+    function obtemDadosDoPaciente(paciente) {
+        return {
+            nome: paciente.querySelector('.info-nome').textContent,
+            peso: parseFloat(paciente.querySelector('.info-peso').textContent),
+            altura: parseFloat(paciente.querySelector('.info-altura').textContent),
+            gordura: paciente.querySelector('.info-gordura').textContent,
+        }
+    }
+
+    function calculaImc(peso, altura) {
+        return peso / (altura * altura);
+    }
+
+    function validaPeso(peso) {
+        if(peso > 0 && peso < 1000) return true;
+    }
+
+    function validaAltura(altura) {
+        if(altura < 3.00) return true;
+    }
+
+    console.log(dadosPaciente);
+
+})();
