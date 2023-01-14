d5a95c0ab2134c1e793583f5bc456f8893f332a1
Vinicius Jose <viniciuswk@gmail.com>
2023-01-14 15:23:36 -0300

    formulário + colocar na lista

--- js/principal.js
+++ js/principal.js
@@ -34,5 +34,51 @@ for (let i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+var botaoAdicionar = document.querySelector("#adicionar-paciente")
+// adiciona um escutador de evento (evento: click, chama: uma função)
+botaoAdicionar.addEventListener("click", function(event) {
+    //mudar comportamento padrão, para que ele execute a ação
+    event.preventDefault();
+    //---------------------------------------------
+    // OBTER VALORES DO FORMULÁRIO
+
+    // selecionando o formulário
+    var form = document.querySelector("#form-adiciona")
+    // para pegar os valores dos campos
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    // --------------------------------------------
+    //  COLOCAR NA PLANILHA
+
+    // criar um elemento
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    // colocando o valor nas td 
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc.toFixed(2);
+
+    // colocar as td(filhos) dentro da tr(pai)
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+    pacienteTr.appendChild(imcTd)
+
+    // colocar o Tr dentro da tabela (tbody) - id:tabela-pacientes
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
+
 
 
